ce6fe4d700d88bef9a36b1504e504cc59b7f9782
fernando <farenasr2511@gmail.com>
2024-05-10 23:25:08 -0600

    ok

---
+++ routs/Carsroutes.js
@@ -0,0 +1,33 @@
+import CarModel from "../models/cars.js";
+const router = express.Router();
+
+router.get("/cars", async (request, response) => {
+    try {
+        const cars = await CarModel.find({});
+        response.send(cars);
+    } catch (error) {
+        response.status(500).send({ error });
+    }
+});
+
+router.post("/cars", async (request, response) => {
+    const product = new CarModel(request.body);
+
+    try {
+        await product.save();
+        response.send(cars);
+    } catch (error) {
+        response.status(500).send(error);
+    }
+});
+
+router.get("/cars/:id", async (request, response) => {
+    try {
+        const cars = await CarModel.findOne({ _id: request.params.id });
+        response.send(cars);
+    } catch (error) {
+        response.status(500).send({ error });
+    }
+});
+
+export default routercars;
---
+++ routs/UsersRoutes.js
@@ -0,0 +1,33 @@
+import UserModel from "../models/users.js";
+const router = express.Router();
+
+router.get("/users", async (request, response) => {
+    try {
+        const users = await UserModel.find({});
+        response.send(users);
+    } catch (error) {
+        response.status(500).send({ error });
+    }
+});
+
+router.post("/users", async (request, response) => {
+    const users = new UserModel(request.body);
+
+    try {
+        await product.save();
+        response.send(users);
+    } catch (error) {
+        response.status(500).send(error);
+    }
+});
+
+router.get("/users/:id", async (request, response) => {
+    try {
+        const users = await UserModel.findOne({ _id: request.params.id });
+        response.send(users);
+    } catch (error) {
+        response.status(500).send({ error });
+    }
+});
+
+export default routerusers;
