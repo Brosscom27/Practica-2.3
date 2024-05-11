9f0c0afc9a037be2300cbef3bd2d1d541aecc0bc
Brosscom27 <mcordovacsx@gmail.com>
2024-05-10 23:21:26 -0600

    Subiendo index

---
+++ index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import dotenv from "dotenv";
+import DB from "./config/db.js";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+DB.connectDB(process.env.DB_URI);
+
+app.use("/users", UsersRoutes);
+app.use("/cars", Carsroutes);
+
+app.listen(port, ()=>{
+    console.log(`Server conect at http://localhost:${port}`);
+});
